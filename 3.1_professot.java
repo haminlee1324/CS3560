e892e87a423b24819914c295c3965afeecc447a1
haminlee1324 <145083454+haminlee1324@users.noreply.github.com>
2025-01-15 02:11:08 -0800

    Create 3.1_professot.java

---
+++ 3.1_professot.java
@@ -0,0 +1,60 @@
+package com.example.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "professor")
+public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;                // PK
+
+    @Column(name = "office_number", nullable = true)
+    private String officeNumber;    // can be null
+
+    @Column(name = "research_area", nullable = false)
+    private String researchArea;
+
+    // Foreign Key to Customer
+    @OneToOne
+    @JoinColumn(name = "customer_id", referencedColumnName = "id", nullable = false)
+    private Customer customer;
+
+    public Professor() { }
+
+    public Professor(String officeNumber, String researchArea) {
+        this.officeNumber = officeNumber;
+        this.researchArea = researchArea;
+    }
+
+    // --- Getters / Setters ---
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getOfficeNumber() {
+        return officeNumber;
+    }
+
+    public void setOfficeNumber(String officeNumber) {
+        this.officeNumber = officeNumber;
+    }
+
+    public String getResearchArea() {
+        return researchArea;
+    }
+
+    public void setResearchArea(String researchArea) {
+        this.researchArea = researchArea;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
