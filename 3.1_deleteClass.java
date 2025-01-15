81ef11612c34c47a20fd930e5f9ed79f511a45e2
haminlee1324 <145083454+haminlee1324@users.noreply.github.com>
2025-01-15 02:14:44 -0800

    Update 3.1_deleteClass.java

--- 3.1_deleteClass.java
+++ 3.1_deleteClass.java
@@ -9,19 +9,15 @@ import com.example.util.HibernateUtil;
 public class MainDelete {
     public static void main(String[] args) {
 
-        // 1. Open a Hibernate Session
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction transaction = null;
         try {
             transaction = session.beginTransaction();
 
-            // Suppose we want to delete a Customer with id=1
             Long customerId = 1L;
 
-            // 2. Retrieve the customer from the DB
             Customer customer = session.get(Customer.class, customerId);
             if (customer != null) {
-                // 3. Delete the customer (cascading will delete the associated Professor as well)
                 session.delete(customer);
                 System.out.println("Deleted Customer with id=" + customerId
                                    + " and its associated Professor.");
@@ -29,7 +25,6 @@ public class MainDelete {
                 System.out.println("Customer with id=" + customerId + " not found.");
             }
 
-            // 4. Commit transaction
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
@@ -37,7 +32,6 @@ public class MainDelete {
             }
             e.printStackTrace();
         } finally {
-            // 5. Close session
             session.close();
             HibernateUtil.getSessionFactory().close();
         }
